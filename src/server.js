a603f28f0de02822985105af6d10a6d98503b650
moonchaewon <codnjs8989@gmail.com>
2021-09-22 18:44:26 +0900

    feat: Add router and architecture

--- src/server.js
+++ src/server.js
@@ -1,38 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter"
+import videoRouter from "./routers/videoRouter"
+import userRouter from "./routers/userRouter"
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1> Not Alllowed </h1>");
-    }
-    console.log("Allowed, you may continue.");
-    next();
-};
-
-const handleHome = (req, res) => {
-    return res.send("I love middelwares");
-};
-
-const handleLogin = (req, res) => {
-    return res.send({ message: "Login here." });
-};
-
-const handleProtected = (req, res) => {
-    return res.send("Welcome to the private lounge.")
-};
-
 app.use(logger);
-app.use(privateMiddleware);
 
-app.get("/", handleHome);
-app.get("/login", handleLogin)
-app.get("/protected", handleProtected)
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => 
     console.log(`Server listening on port http://localhost:${PORT}`);
