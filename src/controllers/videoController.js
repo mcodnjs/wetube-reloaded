acefb3ddc5976e27248293767650bf488660f069
moonchaewon <codnjs8989@gmail.com>
2021-10-14 17:02:50 +0900

    feat:Add using mongo

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,40 +1,80 @@
-const fakeUser = {
-    username: "chaewon",
-    loggedIn: false,
+import Video, { formatHashtags } from "../models/Video";
+
+export const home = async (req, res) => {
+    const videos = await Video.find({}).sort({createdAt: "desc"});
+    console.log(videos);
+    return res.render("home", { pageTitle: videos.title , videos });
 }
-export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            view: 59,
-            id: 1,
-        },
-        {
-            title: "Second Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            view: 59,
-            id: 1,
-        },
-        {
-            title: "Third Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            view: 59,
-            id: 1,
-        },
-    ];
-    return res.render("home", { pageTitle: "Home", fakeUser: fakeUser, videos});
+export const watch = async (req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    return res.render("watch", {pageTitle: `Watching`, video });
+};
+
+export const getEdit = async (req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    if(!video) {
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
+};
+
+export const postEdit = async (req, res) => {
+    const { id } = req.params;
+    const {title, description, hashtags} = req.body;
+    const video = await Video.exists({ _id: id });
+    if(!video) {
+        return res.render("404", { pageTitle: "Video not found." });
+    }
+    await Video.findByIdAndUpdate(id, {
+        title, description, 
+        hashtags: Video.formatHashtags(hashtags),
+    });
+
+    return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+    return res.render("upload", {pageTitle: "Upload Video"});
+};
+
+export const postUpload = async (req, res) => {
+    const {title, description, hashtags } = req.body;
+    try {
+        await Video.create({
+            title,
+            description,
+            hashtags: Video.formatHashtags(hashtags),
+        });    
+        return res.redirect("/");   
+    } catch (error) {
+        console.log(error);
+        return res.render("upload", {  
+            pageTitle: "Upload Vidoe", 
+            errorMessage: error._message,
+        });
+    }
+};
+
+export const deleteVideo = async(req, res) => {
+    const { id } = req.params;
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
 }
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
-}
+
+export const search = async(req, res) => {
+    const { keyword } = req.query;
+    let videos = [];
+    if(keyword) {
+        videos = await Video.find({
+            title: {
+                $regex: new RegExp(`${keyword}$`, "i"),
+            },
+        });
+    }
+    return res.render("search", {pageTitle: "Search", videos});
+};
--- src/server.js
+++ src/server.js
@@ -4,9 +4,8 @@ import globalRouter from "./routers/globalRouter"
 import videoRouter from "./routers/videoRouter"
 import userRouter from "./routers/userRouter"
 
-const PORT = 4000;
 
-console.log(process.cwd());
+// console.log(process.cwd());
 const app = express();
 const logger = morgan("dev");
 
@@ -14,11 +13,9 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 
 app.use(logger);
+app.use(express.urlencoded({ extened: true}));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => 
-    console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
